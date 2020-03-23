1a872b432235f5cfe09557fd8523ee0d90ac20a1
Agustin Gomez <agustin.gomez@devlights.com>
2020-03-22 23:38:52 -0300

    a

--- routes/cards.js
+++ routes/cards.js
@@ -1,20 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const Cards = require("./../models/cards");
+const multer = require("multer");
+
+const upload = multer({ dest: "uploads/" });
 
 router.get("/", async (req, res) => {
 	try {
-		const cards = await Cards.find({});
+		const cards = await Cards.find();
 		res.status(200).json(cards);
 	} catch (error) {
 		res.status(200).json({ data: error.message });
 	}
 });
 
-router.post("/", async (req, res) => {
+router.post("/", upload.single("image"), async (req, res) => {
 	try {
-		Cards.create(req.body);
-		await res.status(200).json({ data: "Card Cargada", error: false });
+		// console.log(req.body);
+		console.log(req.file);
+
+		// Cards.create(req.body);
+		res.status(200).json({ data: "Card Cargada", error: false });
 	} catch (error) {
 		res.status(500).json({ data: error.message });
 	}
