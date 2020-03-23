51c622f32864366e89aad9c258d3391f2b8f7982
Agustin Gomez <agustin.gomez@devlights.com>
2020-03-23 14:01:38 -0300

    Saliooooooo

--- routes/cards.js
+++ routes/cards.js
@@ -3,7 +3,16 @@ const router = express.Router();
 const Cards = require("./../models/cards");
 const multer = require("multer");
 
-const upload = multer({ dest: "uploads/" });
+const storage = multer.diskStorage({
+	destination: function(req, file, cb) {
+		cb(null, "./uploads/");
+	},
+	filename: function(req, file, cb) {
+		cb(null, file.originalname);
+	}
+});
+
+const upload = multer({ storage: storage });
 
 router.get("/", async (req, res) => {
 	try {
@@ -16,10 +25,14 @@ router.get("/", async (req, res) => {
 
 router.post("/", upload.single("image"), async (req, res) => {
 	try {
-		// console.log(req.body);
-		console.log(req.file);
-
-		// Cards.create(req.body);
+		const { title, subtitle, description, link } = req.body;
+		Cards.create({
+			title: title,
+			subtitle: subtitle,
+			description: description,
+			link: link,
+			image: req.file.filename
+		});
 		res.status(200).json({ data: "Card Cargada", error: false });
 	} catch (error) {
 		res.status(500).json({ data: error.message });
